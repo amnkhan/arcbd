c7bbe6583ffb156822892aac9577c12083e43d91
Al Amin Khan <amnkhan.me@gmail.com>
2017-10-27 22:29:21 +0600

    arcbd.org #011 | owl carosuel added
    Owl carousel added

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Owl carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        navText: ['<i class="fa fa-angle-left"></i>' , '<i class="fa fa-angle-right"></i>'],
+        responsiveClass:true,
+        animateOut: 'fadeOut',
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
